1a73efda38e4d6914eeae93d0ebeddd1663690b1
Миша <ruhose73@gmail.com>
2023-01-11 23:46:40 +0300

    fix smth

--- src/modules/auth/dto/token.dto.ts
+++ src/modules/auth/dto/token.dto.ts
@@ -1,18 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { AccessToken, RefreshToken } from '../interfaces';
 
 export class TokenDto {
   @IsNotEmpty()
   @ApiProperty({
-    example: 'd4e5cbd9-b96c-4543-9f53-24a01693bc76',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIxYTFhY2M3LTkzMTUtNDFiNi05Y2NjLTZhMGY5M2EzYmZ',
     description: `access token`,
   })
   accessToken: AccessToken;
 
   @IsNotEmpty()
   @ApiProperty({
-    example: 'd4e5cbd9-b96c-4543-9f53-24a01693bc76',
+    example: '3c4edc65-d1e0-4f10-84f6-442d50a39db5',
     description: `refresh token`,
   })
   refreshToken: RefreshToken;
