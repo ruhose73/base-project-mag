154b36d8c7f4d55337a1dde6c15c30ce8413c2a3
Миша <ruhose73@gmail.com>
2023-06-02 16:15:56 +0300

    add activation user functionality

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserCreationAttributes } from './interfaces';
 import { User } from './model/user.model';
+import { UserDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -17,5 +18,13 @@ export class UserService {
 
   async saveUser(dto: UserCreationAttributes): Promise<User | null> {
     return await this.userRepository.save(dto);
+  }
+
+  async activateUser(link:string): Promise<UserDto| null>  {
+    const user:User = await this.userRepository.save({
+      link:link,
+      isActive:true
+    });
+    return new UserDto(user)
   }
 }
